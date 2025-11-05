765bee32d4eacc50ec58d4fc7709c1e47eed9982
inaradfence <cyberdfence3@gmail.com>
2025-11-05 09:51:29 +0500

    order stats and order widgets completed

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -14,6 +16,27 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+          null => Tab::make('All'),
+          'new' => Tab::make()->query(fn ($query) => $query->where('status', 'new')),
+          'processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing')),
+          'delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered')),
+          'shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped')),
+          'cancelled' => Tab::make()->query(fn ($query) => $query->where('status', 'cancelled')),
+
+          
         ];
     }
 }
